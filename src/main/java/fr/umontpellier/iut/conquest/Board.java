3bce626dad55acbf22426e7076838c70e01445a3
Florain-Ravet-Lecourt <florian.ravet-lecourt@etu.umontpellier.fr>
2019-12-26 15:44:37 +0100

    getnbpawns

--- src/main/java/fr/umontpellier/iut/conquest/Board.java
+++ src/main/java/fr/umontpellier/iut/conquest/Board.java
@@ -163,8 +163,8 @@ public class Board {
         List<Move> validMoves = new ArrayList<>();
 
         // on parcourt toutes les cases
-        for (int startingRow = 0; startingRow <= field.length; startingRow++) {
-            for (int startingColumn = 0; startingColumn <= field.length; startingColumn++) {
+        for (int startingRow = 0; startingRow < field.length; startingRow++) {
+            for (int startingColumn = 0; startingColumn < field.length; startingColumn++) {
                 
                 // on vérifie si c'est une case qui appartient au joeur actuel 
                 if (validPlayer(field[startingRow][startingColumn], player))
@@ -179,10 +179,8 @@ public class Board {
      */
     public int getNbPawns(Player player) {
         int nombre = 0;
-        int i = 0;
-        int j = 0;
-        while(i <= field.length-1) {
-            while(j <= field.length-1) {
+        for(int i = 0; i <= field.length-1; i++){
+            for(int j = 0; j <= field.length-1; j++) {
                 if(validPlayer(field[i][j], player)){
                     nombre++;
                 }

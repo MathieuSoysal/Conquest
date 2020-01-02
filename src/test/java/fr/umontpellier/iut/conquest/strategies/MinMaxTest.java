6eb157157c7453d0c684f21e4aac2c0c840c8988
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-01-02 23:56:43 +0100

    Ajout : MinMaxTest

---
+++ src/test/java/fr/umontpellier/iut/conquest/strategies/MinMaxTest.java
@@ -0,0 +1,34 @@
+package fr.umontpellier.iut.conquest.strategies;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import fr.umontpellier.iut.conquest.Pawn;
+import fr.umontpellier.iut.conquest.Player;
+import fr.umontpellier.iut.conquest.board.Board;
+import fr.umontpellier.iut.conquest.board.Move;
+
+public class MinMaxTest {
+
+    @Test
+    public void getmove_with_just_one_Pawn_with_anticipation_1() {
+        MinMax myRobot = new MinMax(1);
+        Player player1 = new Player(myRobot, null, null, 1);
+        Player player2 = new Player(null, null, null, 2);
+        Pawn pawn = new Pawn(player1);
+        Pawn pawn2 = new Pawn(player2);
+
+        Pawn[][] field = { // field :
+                { pawn, null, pawn2 }, // row 0
+                { null, null, null }, // row 1
+                { pawn2, null, pawn } // row 2
+        };
+        Board board = new Board(field);
+        Move move = myRobot.getMove(board, player1);
+        assertTrue("move non valide :",board.isValid(move, player1));
+        board.movePawn(move);
+        assertEquals(board.toString(),5,board.getNbPawns(player1));
+    }
+}

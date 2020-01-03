a784501f830ebe89864f60003b72b164c98526a4
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-01-03 23:54:45 +0100

    Ajout test : SpeculationTest.java :  - Ajout du test qui vérifie si l'IA Speculation prend la décision la plus aventageuse

--- src/test/java/fr/umontpellier/iut/conquest/strategies/SpeculationTest.java
+++ src/test/java/fr/umontpellier/iut/conquest/strategies/SpeculationTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import fr.umontpellier.iut.conquest.Pawn;
@@ -38,7 +39,7 @@ public class SpeculationTest {
     }
 
     @Test
-    public void getmove_with_just_one_Pawn_and_two_pawns_other_player_with_anticipation_2() {
+    public void getmove_with_just_one_Pawn_and_two_pawns_other_player() {
         Speculation myRobot = new Speculation();
         Player player1 = new Player(myRobot, null, null, 1);
         Player player2 = new Player(null, null, null, 2);
@@ -59,5 +60,31 @@ public class SpeculationTest {
         assertTrue("move non valide :", board.isValid(move, player1));
         board.movePawn(move);
         assertEquals(board.toString(), 4, board.getNbPawns(player1));
+    }
+
+    @Ignore
+    @Test
+    public void getmove_with_just_one_Pawn_and_two_pawns_other_player_when_player_can_kill_all() {
+        Speculation myRobot = new Speculation();
+        Player player1 = new Player(myRobot, null, null, 1);
+        Player player2 = new Player(null, null, null, 2);
+        Pawn pawn = new Pawn(player1);
+        Pawn pawn2 = new Pawn(player2);
+
+        Pawn[][] field = { // field :
+                { null, null, null, null, null, null, null }, // row 0
+                { null, null, null, null, null, null, null }, // row 1
+                { null, null, null, null, null, pawn2, null }, // row 2
+                { null, null, null, pawn, null, null, pawn2}, // row 3
+                { null, null, null, null, null, null, null }, // row 4
+                { null, null, null, null, null, null, null }, // row 5
+                { null, null, null, null, null, null, null }, // row 6
+        };
+        Board board = new Board(field);
+        Move move = myRobot.getMove(board, player1);
+        assertTrue("move non valide :", board.isValid(move, player1));
+        board.movePawn(move);
+        assertEquals(board.toString(), 3, board.getNbPawns(player1));
+        assertEquals(board.toString(), 0, board.getNbPawns(player2));
     }
 }

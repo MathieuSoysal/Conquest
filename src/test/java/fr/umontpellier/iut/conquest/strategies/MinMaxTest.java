d601ff91c85568f8df343abe32455b445c2e09b6
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-01-04 11:46:53 +0100

    Ajout tests : MinMaxTest :  - Ajout de plusieurs tests vérifiant le comportement de MinMax avec un IALevel de 4

--- src/test/java/fr/umontpellier/iut/conquest/strategies/MinMaxTest.java
+++ src/test/java/fr/umontpellier/iut/conquest/strategies/MinMaxTest.java
@@ -214,4 +214,81 @@ public class MinMaxTest {
         board.movePawn(move);
         assertNotEquals(board.toString(), null, board.getField()[1][4]);
     }
+
+    // ATTENTION se test est très long à executer et l'IA ne valide pas le test
+    @Ignore
+    @Test
+    public void getmove_should_is_optimum_with_five_Pawns_and_three_pawns_other_player_with_IAlevel4() {
+        MinMax myRobot = new MinMax(4);
+        Player player1 = new Player(myRobot, null, null, 1);
+        Player player2 = new Player(null, null, null, 2);
+        Pawn pawn = new Pawn(player1);
+        Pawn pawn2 = new Pawn(player2);
+
+        Pawn[][] field = { // field :
+                { pawn, null, null, null, null }, // row 0
+                { null, null, pawn2, pawn2, null }, // row 1
+                { null, null, pawn2, null, null }, // row 2
+                { null, pawn, null, null, null }, // row 3
+                { pawn, pawn, null, null, pawn }, // row 4
+        };
+        Board board = new Board(field);
+        Move move = myRobot.getMove(board, player1);
+        assertTrue("move non valide :", board.isValid(move, player1));
+        board.movePawn(move);
+        assertEquals(board.toString(), 7, board.getNbPawns(player1));
+        assertEquals(board.toString(), 0, board.getNbPawns(player2));
+    }
+
+    @Ignore
+    @Test
+    public void getmove_with_one_Pawn_and_two_pawns_other_player_when_player_can_kill_all() {
+        MinMax myRobot = new MinMax(4);
+        Player player1 = new Player(myRobot, null, null, 1);
+        Player player2 = new Player(null, null, null, 2);
+        Pawn pawn = new Pawn(player1);
+        Pawn pawn2 = new Pawn(player2);
+
+        Pawn[][] field = { // field :
+                { null, null, null, null, null, null, null }, // row 0
+                { null, null, null, null, null, null, null }, // row 1
+                { null, null, null, null, null, pawn2, null }, // row 2
+                { null, null, null, pawn, null, null, pawn2 }, // row 3
+                { null, null, null, null, null, null, null }, // row 4
+                { null, null, null, null, null, null, null }, // row 5
+                { null, null, null, null, null, null, null }, // row 6
+        };
+        Board board = new Board(field);
+        Move move = myRobot.getMove(board, player1);
+        assertTrue("move non valide :", board.isValid(move, player1));
+        board.movePawn(move);
+        assertEquals(board.toString(), 3, board.getNbPawns(player1));
+        assertEquals(board.toString(), 0, board.getNbPawns(player2));
+    }
+
+    // TODO à voir si vraimment utile
+    @Ignore
+    @Test
+    public void getmove_is_optimum_move_with_just_three_Pawsn_and_three_pawns_other_player_with_IAlevel4() {
+        MinMax myRobot = new MinMax(4);
+        Player player1 = new Player(myRobot, null, null, 1);
+        Player player2 = new Player(null, null, null, 2);
+        Pawn pawn = new Pawn(player1);
+        Pawn pawn2 = new Pawn(player2);
+
+        Pawn[][] field = { // field :
+                { pawn, null, null, null, pawn2 }, // row 0
+                { null, null, null, null, null }, // row 1
+                { null, null, null, null, null }, // row 2
+                { pawn2, null, null, pawn, null }, // row 3
+                { pawn2, null, null, null, pawn }, // row 4
+        };
+        Board board = new Board(field);
+        Move move = myRobot.getMove(board, player1);
+        assertTrue("move non valide :", board.isValid(move, player1));
+        board.movePawn(move);
+        assertEquals(board.toString(), 4, board.getNbPawns(player1));
+        assertEquals(board.toString(), 1, board.getNbPawns(player2));
+    }
+
 }

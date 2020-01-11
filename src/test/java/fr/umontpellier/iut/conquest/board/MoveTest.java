28bece9f2eb6b753245da04478fa70fc64c6d909
MathieuSoysal <Mathieu.soysal@outlook.fr>
2020-01-11 17:39:47 +0100

    Ajout : MoveTest :  - Coverage des méthodes de la class Move

---
+++ src/test/java/fr/umontpellier/iut/conquest/board/MoveTest.java
@@ -0,0 +1,76 @@
+package fr.umontpellier.iut.conquest.board;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsNot;
+import org.junit.Test;
+
+public class MoveTest {
+
+
+    @Test
+    public void test_Equals_with_same_address() {
+        Move move = new Move(1,1,1,1);
+        Move move2 = move;
+        assertThat(move , Is.is(move2));
+    }
+
+    @Test
+    public void test_Equals_with_same_values() {
+        Move move = new Move(1,1,1,1);
+        Move move2 = new Move(1,1,1,1);
+        assertThat(move , Is.is(move2));
+    }
+
+    @Test
+    public void test_not_Equals_with_null() {
+        Move move = new Move(1,1,1,1);
+        Move move2 = null;
+        assertThat(move , IsNot.not(move2));
+    }
+
+    @Test
+    public void test_not_Equals_with_other_class() {
+        Move move = new Move(1,1,1,1);
+        String move2 = "1 1 1 1";
+        assertThat(move , IsNot.not(move2));
+    }
+
+    
+    @Test
+    public void test_not_Equals_with_not_same_arrival_column() {
+        Move move = new Move(1,1,1,1);
+        Move move2 = new Move(1,1,1,2);
+        assertThat(move , IsNot.not(move2));
+    }
+
+    @Test
+    public void test_not_Equals_with_not_same_arrival_row() {
+        Move move = new Move(1,1,1,1);
+        Move move2 = new Move(1,1,3,1);
+        assertThat(move , IsNot.not(move2));
+    }
+
+    @Test
+    public void test_not_Equals_with_not_same_starting_row() {
+        Move move = new Move(1,1,1,1);
+        Move move2 = new Move(2,1,1,1);
+        assertThat(move , IsNot.not(move2));
+    }
+
+    @Test
+    public void test_not_Equals_with_not_same_starting_column() {
+        Move move = new Move(1,1,1,1);
+        Move move2 = new Move(1,2,1,1);
+        assertThat(move , IsNot.not(move2));
+    }
+
+    @Test
+    public void test_same_hashcode_with_same_values() {
+        int move = new Move(1,1,1,1).hashCode();
+        int move2 = new Move(1,1,1,1).hashCode();
+        assertEquals(move , move2);
+    }
+}
